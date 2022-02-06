0ac92ace533a72c56b3a1e500ac756f240329eac
mtinti <michele.tinti@gmail.com>
2022-02-06 09:02:34 +0000

    start

---
+++ config.js
@@ -0,0 +1,29 @@
+let X1='logFC';
+let Y1='FDR';
+let X2='log_AveExpr';
+let Y2='logFC';
+
+let FLIP_Y1=true;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='CFB2';
+
+let margin = 0.05;
+
+let BOX_PLOT_COLS_1 = ['146P1','146P2','146P3']
+let BOX_PLOT_COLS_2 = ['146M1','146M2','146M3']
+
+let CONDITION_1 = 'Plus'
+let CONDITION_2 = 'Minus'
+
+
+let COLOR_SELECTION_TABLE = 'orange';
+let COLOR_SELECTION_CIRCLE = 'orange';
+
+let Y_LABEL_BARPLOT = 'Intensity';
+
+let File_1 = 'indata_146.csv';
+let File_2 = 'indata2_146.csv';
